7a829da3cd60b8e22de1b18c6bcaf85431ec8f57
3milyLiu <emily-liu@live.co.uk>
2019-10-01 15:03:05 +0100

    add CornerSeekingAgent
    Lab 2
    
    to add:
    - moving after all corners have been reached

--- sampleAgents.py
+++ sampleAgents.py
@@ -188,3 +188,129 @@ class SurvivalAgent( Agent ):
 
     def ghostScore( self, pacman, ghosts ):
         return min( [ util.manhattanDistance( pacman, ghost ) for ghost in ghosts ] )
+
+class CornerSeekingAgent(Agent):
+
+    def __init__(self):
+         self.BL = False
+         self.TL = False
+         self.BR = False
+         self.TR = False
+
+    def final(self, state):
+         self.BL = False
+         self.TL = False
+         self.BR = False
+         self.TR = False
+
+    def getAction(self, state):
+
+        # Get extreme x and y values for the grid
+        corners = api.corners(state)
+        print corners
+        # Setup variable to hold the values
+        minX = 100
+        minY = 100
+        maxX = 0
+        maxY = 0
+
+        # Sweep through corner coordinates looking for max and min
+        # values.
+        for i in range(len(corners)):
+            cornerX = corners[i][0]
+            cornerY = corners[i][1]
+
+            if cornerX < minX:
+                minX = cornerX
+            if cornerY < minY:
+                minY = cornerY
+            if cornerX > maxX:
+                maxX = cornerX
+            if cornerY > maxY:
+                maxY = cornerY
+
+        legal = api.legalActions(state)
+        print legal
+        if Directions.STOP in legal:
+            legal.remove(Directions.STOP)
+        pacman = api.whereAmI(state)
+        #print pacman
+
+        # bottom left corner
+        # check
+        if pacman[0] == minX + 1:
+            if pacman[1] == minY + 1:
+                print "Got to BL!"
+                self.BL = True
+
+        # bottom left corner. West then south
+        if self.BL == False:
+            if pacman[0] > minX + 1:
+                if Directions.WEST in legal:
+                    return api.makeMove(Directions.WEST, legal)
+                else:
+                    pick = random.choice(legal)
+                    return api.makeMove(pick, legal)
+            else:
+                if Directions.SOUTH in legal:
+                    return api.makeMove(Directions.SOUTH, legal)
+                else:
+                    pick = random.choice(legal)
+                    return api.makeMove(pick, legal)
+
+        # top left corner
+        # check
+        if pacman[0] == minX + 1:
+           if pacman[1] == maxY - 1:
+                print "Got to TL!"
+                self.TL = True
+
+        # top left corner. West then North.
+        if self.TL == False:
+            if pacman[0] > minX + 1:
+                if Directions.WEST in legal:
+                    return api.makeMove(Directions.WEST, legal)
+                else:
+                    pick = random.choice(legal)
+                    return api.makeMove(pick, legal)
+            else:
+                if Directions.NORTH in legal:
+                    return api.makeMove(Directions.NORTH, legal)
+                else:
+                    pick = random.choice(legal)
+                    return api.makeMove(pick, legal)
+
+        # top right corner
+        # check
+        if pacman[0] == maxX - 1:
+           if pacman[1] == maxY - 1:
+                print "Got to TR!"
+                self.TR = True
+
+        # top right corner. East then north
+        if self.TR == False:
+            if pacman[0] < maxX - 1:
+                if Directions.EAST in legal:
+                    return api.makeMove(Directions.EAST, legal)
+                else:
+                    pick = random.choice(legal)
+                    return api.makeMove(pick, legal)
+            else:
+                if Directions.NORTH in legal:
+                    return api.makeMove(Directions.NORTH, legal)
+                else:
+                    pick = random.choice(legal)
+                    return api.makeMove(pick, legal)
+
+        # bottom right corner (south)
+        if pacman[0] == maxX - 1:
+           if pacman[1] == minY + 1:
+                print "Got to BR!"
+                self.BR = True
+                return api.makeMove(Directions.STOP, legal)
+           else:
+               print "Nearly there"
+               return api.makeMove(Directions.SOUTH, legal)
+
+        print "Not doing anything!"
+        return api.makeMove(Directions.STOP, legal)
